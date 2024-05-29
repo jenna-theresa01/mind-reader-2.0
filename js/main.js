7c6a31cab01b0b5413fa4fdcbe73836921820d0a
Jenna Martin <jennamar828@gmail.com>
2024-05-29 13:04:35 -0400

    started psuedocode and added files

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/*
+Start
+    "I can read you mind" start container
+    go button
+change state without refreshing the page
+    "Pick a number" container
+        next button 
+        reset button
+        "when you have a number click next"
+    "Add digits to get a new number"
+        next button
+        reset button
+        Example below next button
+    "Subtract new number from original number"
+        next button
+        reset button
+        Example below next button
+    "Find your new number and corresponding symbols"
+        symbols page
+        reveal button
+        reset button
+    "This is your symbol" 
+        reset button
+END
+*/
+
+/* 
+Do I want to use vanilla js or do I want to use React to finish this project?
+This is where I started with psuedocode during mind reader 1.0:
+     Functions I might want to use
+    - window.onload function
+        - 
+    - addEventListener for the next and reset buttons
+        - oncuechange?? fires a TextTrack has changed the currently displaying cues 
+        - "click event"
+    - a function to switch between "pages" without refreshing the webpage and using a button 
+        -toggle (hide/show) an element
+    - onclick listener
+
+*/
---
+++ pseudocode.md
@@ -0,0 +1,15 @@
+## Mind Reader Project 2.0
+----------------------------------------------------------------------------------------------------------------------------
+
+#### Requirements
+- [ ] Pseudocode first
+- [ ] Use JS to show the transition from one view to the next view
+- [ ] Show state management for each step of the single page app
+    - [ ] only create single index.html
+    - [ ] no page refresh
+    - [ ] update state using a variable that tells the computer what 'view' it is 
+    - [ ] use functions to move forward a view
+    - [ ] use functions to reset to the first view
+    - [ ] use functions to display the correct view on load / after every state change
+- [ ] use icons 
+- [ ] aal multiples of 9 should be the same symbol
